9dcc550e33223add103ca8a1be350a4082ea3b3a
SONGJAEHEON <spes2you@gmail.com>
2023-03-26 22:27:02 +0900

    feat: spring boot init, react 연동(CORS)

---
+++ src/main/java/kr/co/suitcarrier/web/controller/IndexController.java
@@ -0,0 +1,18 @@
+package kr.co.suitcarrier.web.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    // react에서 localhost:3000/api/hello 호출 시에 사용
+    @GetMapping("/api/hello")
+    // CORS 허용
+    @CrossOrigin(origins = "*", methods = RequestMethod.GET)
+    public String test() {
+        return "Hello, world!";
+    }
+}
