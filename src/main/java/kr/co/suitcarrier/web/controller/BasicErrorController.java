0242579df1f658d4835e311945ec1c4945663270
GiYeons <65033360+GiYeons@users.noreply.github.com>
2023-05-07 05:14:46 +0900

    feat: swagger3 기본 설정

--- src/main/java/kr/co/suitcarrier/web/controller/BasicErrorController.java
+++ src/main/java/kr/co/suitcarrier/web/controller/BasicErrorController.java
@@ -1,9 +1,11 @@
 package kr.co.suitcarrier.web.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Tag(name = "basicError", description = "기본 에러 처리 API")
 @Controller
 public class BasicErrorController implements ErrorController {
     @RequestMapping("/error")
