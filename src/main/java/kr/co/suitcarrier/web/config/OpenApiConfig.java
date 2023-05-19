f1212ef36c747fd68b5a314873e3d53b24c175c5
songjaeheon <spes2you@gmail.com>
2023-05-19 10:00:48 +0900

    style: 미사용 코드 제거

--- src/main/java/kr/co/suitcarrier/web/config/OpenApiConfig.java
+++ src/main/java/kr/co/suitcarrier/web/config/OpenApiConfig.java
@@ -6,13 +6,9 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Component
 public class OpenApiConfig {
