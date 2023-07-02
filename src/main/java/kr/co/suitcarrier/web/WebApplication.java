736cfecc34d8d55e9c1fe9d6c40930f93e3a14c5
박상현 <shawnpark98@naver.com>
2023-07-02 09:47:10 +0900

    feat: 찜 API 구현

--- src/main/java/kr/co/suitcarrier/web/WebApplication.java
+++ src/main/java/kr/co/suitcarrier/web/WebApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 // Spring Boot에서 로그인 관련 처리가 없을 때 작동하는 기본 로그인을 비활성화, Redis 리퍼지토리 설정 비활성화
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class, RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class })
@@ -17,5 +19,11 @@ public class WebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
-	
+
+	// @DeleteMapping, @PutMapping 등의 최신 매핑기능을 이용하기 위해 추가
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+		return new HiddenHttpMethodFilter();
+	}	
+
 }
