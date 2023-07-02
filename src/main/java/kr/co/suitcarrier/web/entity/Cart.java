6b0ab62a88842fdf94826918b5404cbc063ad3c4
박상현 <shawnpark98@naver.com>
2023-07-02 10:13:17 +0900

    feat: 장바구니 API 구현

--- src/main/java/kr/co/suitcarrier/web/entity/Cart.java
+++ src/main/java/kr/co/suitcarrier/web/entity/Cart.java
@@ -2,11 +2,13 @@ package kr.co.suitcarrier.web.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Setter
 @Table(name = "cart")
 public class Cart {
     @Id
