446da817edd7decccf6930994288aaa3084b286b
박상현 <shawnpark98@naver.com>
2023-07-02 09:47:10 +0900

    feat: 찜 API 구현

---
+++ src/main/java/kr/co/suitcarrier/web/dto/LikeRequestDto.java
@@ -0,0 +1,13 @@
+package kr.co.suitcarrier.web.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Schema(description = "찜")
+@Getter
+public class LikeRequestDto {
+
+    private Integer user;
+    private Integer post;
+
+}
--- src/main/java/kr/co/suitcarrier/web/repository/LikeRepository.java
+++ src/main/java/kr/co/suitcarrier/web/repository/LikeRepository.java
@@ -1,11 +1,24 @@
 package kr.co.suitcarrier.web.repository;
 
 import kr.co.suitcarrier.web.entity.Like;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
+    @Query(value = "SELECT * FROM like WHERE user_id = :userId)", nativeQuery = true)
+    List<Like> findByUser(Integer userId);
+
+    @Query(value = "SELECT * FROM like WHERE user_id = :userId AND post_id = :postId)", nativeQuery = true)
+    Optional<Like> findByUserAndPost(Integer userId, Integer postId);
+
+    @Query(value = "DELETE FROM like WHERE user_id = :userId AND post_id = :postId)", nativeQuery = true)
+    void deleteByUserAndPost(Integer userId, Integer postId);
 
 }
