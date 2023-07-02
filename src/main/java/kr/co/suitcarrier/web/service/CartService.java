17971941413a9ea56adf8edf58dccf2b5547a67e
박상현 <shawnpark98@naver.com>
2023-07-02 10:13:17 +0900

    feat: 장바구니 API 구현

---
+++ src/main/java/kr/co/suitcarrier/web/service/CartService.java
@@ -0,0 +1,66 @@
+package kr.co.suitcarrier.web.service;
+
+import kr.co.suitcarrier.web.dto.CartRequestDto;
+import kr.co.suitcarrier.web.entity.Cart;
+import kr.co.suitcarrier.web.repository.CartRepository;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+
+    private final CartRepository cartRepository;
+
+    @Transactional
+    public List<Cart> getCartList(Integer userId) {
+        try {
+            return cartRepository.findByUser(userId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return List.of();
+        }
+    }
+
+    @Transactional
+    public boolean createCartItem(
+        Integer userId, 
+        Integer postId, 
+        LocalDateTime rentDate, 
+        LocalDateTime returnDate, 
+        Integer rentPossible
+    ) {
+        try {
+            Cart cartEntity = new Cart();
+            cartEntity.setUser(userId);
+            cartEntity.setPost(postId);
+            cartEntity.setRentDate(rentDate);
+            cartEntity.setReturnDate(returnDate);
+            cartEntity.setRentPossible(rentPossible);
+            cartRepository.save(cartEntity);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Transactional
+    public boolean deleteCartItem(Integer cartId) {
+        try {
+            cartRepository.deleteById(cartId);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
