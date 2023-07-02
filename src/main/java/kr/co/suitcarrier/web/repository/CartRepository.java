3a888afaa7cc056ad8bc06f96180fff6903aa68d
박상현 <shawnpark98@naver.com>
2023-07-02 14:27:22 +0900

    feat: 장바구니 중복 처리

--- src/main/java/kr/co/suitcarrier/web/repository/CartRepository.java
+++ src/main/java/kr/co/suitcarrier/web/repository/CartRepository.java
@@ -6,13 +6,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     @Query(value = "SELECT * FROM cart WHERE user_id = :userId)", nativeQuery = true)
     List<Cart> findByUser(Integer userId);
+
+    @Query(value = "SELECT * " +
+                    "FROM cart " +
+                    "WHERE post_id = :postId" +
+                    "AND (" + 
+                        "(:rentDate BETWEEN rent_date AND return_date)" +
+                        "OR (:returnDate BETWEEN rent_date AND return_date)" +
+                    ")", 
+                    nativeQuery = true)
+    Optional<Cart> findByPostAndDateRange(Integer postId, LocalDateTime rentDate, LocalDateTime returnDate);
 
     @Query(value = "DELETE FROM cart WHERE id = :cartId)", nativeQuery = true)
     void deleteById(Integer cartId);
