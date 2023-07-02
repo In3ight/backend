de39e0ef3716bedc34f4802b8d586e499e6af8b9
박상현 <shawnpark98@naver.com>
2023-07-02 16:34:58 +0900

    feat: 주문조회 API 구현

--- src/main/java/kr/co/suitcarrier/web/repository/OrderRepository.java
+++ src/main/java/kr/co/suitcarrier/web/repository/OrderRepository.java
@@ -1,11 +1,17 @@
 package kr.co.suitcarrier.web.repository;
 
 import kr.co.suitcarrier.web.entity.Order;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    @Query(value = "SELECT * FROM order WHERE user_id = :userId)", nativeQuery = true)
+    List<Order> findByUser(Integer userId);
 
 }
