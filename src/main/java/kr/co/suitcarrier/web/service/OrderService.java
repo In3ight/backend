1b43e7768c04735f4215159c0103dc7cdeb7641a
박상현 <shawnpark98@naver.com>
2023-07-10 15:19:50 +0900

    feat: 주문하기 API 구현

--- src/main/java/kr/co/suitcarrier/web/service/OrderService.java
+++ src/main/java/kr/co/suitcarrier/web/service/OrderService.java
@@ -2,14 +2,17 @@ package kr.co.suitcarrier.web.service;
 
 import kr.co.suitcarrier.web.entity.Order;
 import kr.co.suitcarrier.web.entity.OrderState;
+import kr.co.suitcarrier.web.entity.User;
 import kr.co.suitcarrier.web.repository.OrderRepository;
 import kr.co.suitcarrier.web.repository.OrderStateRepository;
+import kr.co.suitcarrier.web.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +22,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderStateRepository orderStateRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public List<Order> getOrderList(Integer userId) {
@@ -37,6 +41,28 @@ public class OrderService {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    @Transactional
+    public boolean createOrder(String userEmail, String finalPrice, LocalDateTime rentDate, LocalDateTime createdAt) {
+        try {
+            Order orderEntity = new Order();
+
+            User user = userRepository.findByEmail(userEmail).get();
+            orderEntity.setUser(user);
+
+            orderEntity.setFinalPrice(finalPrice);
+            orderEntity.setRentDate(rentDate);
+            orderEntity.setCreatedAt(createdAt);
+            orderEntity.setUpdatedAt(createdAt);
+            orderEntity.setIsDeleted(0);
+
+            orderRepository.save(orderEntity);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
         }
     }
 
