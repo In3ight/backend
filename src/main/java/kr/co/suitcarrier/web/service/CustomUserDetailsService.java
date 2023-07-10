a5eb11010a792fcf0e0b9dd7f98785b64ab633b2
박상현 <shawnpark98@naver.com>
2023-07-10 14:54:03 +0900

    fix: path 네이밍 변경, SecurityContextHolder 사용, ResponseDTO 생성

--- src/main/java/kr/co/suitcarrier/web/service/CustomUserDetailsService.java
+++ src/main/java/kr/co/suitcarrier/web/service/CustomUserDetailsService.java
@@ -29,6 +29,7 @@ public class CustomUserDetailsService implements UserDetailsService {
             user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
             
             // Set the user details
+            customUserDetails.setUuid(user.getId().toString());
             customUserDetails.setUuid(user.getUuid().toString());
             customUserDetails.setEmail(user.getEmail());
             customUserDetails.setPassword(user.getPassword());
