e2678398a83c679815ff3cb300bcd509a67ff6c0
GiYeons <65033360+GiYeons@users.noreply.github.com>
2023-07-13 01:43:24 +0900

    feat: 리뷰 작성 구현

--- src/main/java/kr/co/suitcarrier/web/repository/ReviewRepository.java
+++ src/main/java/kr/co/suitcarrier/web/repository/ReviewRepository.java
@@ -1,6 +1,6 @@
 package kr.co.suitcarrier.web.repository;
 
-import kr.co.suitcarrier.web.entity.Review;
+import kr.co.suitcarrier.web.entity.post.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
