f1412cd48c10984b10045e85d1e699195686d223
GiYeons <65033360+GiYeons@users.noreply.github.com>
2023-07-16 03:26:44 +0900

    feat: 게시글 목록 조회 및 권한처리
    refactor: 파일 위치 변경

--- src/main/java/kr/co/suitcarrier/web/config/SecurityConfiguration.java
+++ src/main/java/kr/co/suitcarrier/web/config/SecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,6 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
     
     @Value("${security.enable-csrf:true}")
--- src/main/java/kr/co/suitcarrier/web/controller/PostController.java
+++ src/main/java/kr/co/suitcarrier/web/controller/PostController.java
@@ -1,13 +1,12 @@
 package kr.co.suitcarrier.web.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import kr.co.suitcarrier.web.dto.*;
+import kr.co.suitcarrier.web.dto.post.*;
 import kr.co.suitcarrier.web.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,38 +15,48 @@ import java.util.List;
 public class PostController {
     private final PostService postService;
 
-    // 게시글 생성
     @PostMapping("/create")
+    @Operation(summary = "게시글 생성")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> createPost(@RequestBody PostCreateRequestDto requestDto) {
         return postService.createPost(requestDto);
     }
 
     @GetMapping("/{postId}/delete")
+    @Operation(summary = "게시글 삭제")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> deletePost(@PathVariable("postId") Long postId) {
         return postService.deletePost(postId);
     }
 
-    // 게시글 상세조회
     @GetMapping("/{postId}")
+    @Operation(summary = "게시글 상세조회")
     public ResponseEntity<PostResponseDto> getPost(@PathVariable("postId") Long postId) {
         return postService.getPost(postId);
     }
 
-    // 매물 검색
+    @GetMapping("/list")
+    @Operation(summary = "게시글 목록 조회(카드형)", description = "필요에 따라 여러가지 조회 방법 추가할 예정")
+    public ResponseEntity<ListingResponseDto> listingPosts() {
+        return postService.listingPosts();
+    }
+
     @PostMapping("/search")
     @Operation(summary = "매물 검색", description = "size, color, brand의 경우, '선택 없음'->null로 처리할 것.")
     public ResponseEntity<SearchResponseDto> searchPosts(@RequestBody SearchRequestDto requestDto) {
         return postService.searchPosts(requestDto);
     }
 
-    // 게시글 리뷰 작성
     @PostMapping("/{postId}/createReview")
+    @Operation(summary = "게시글 리뷰 작성")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> createReview( @PathVariable("postId") Long postId, @RequestBody ReviewCreateRequestDto requestDto) {
         return postService.createReview(requestDto, postId);
     }
 
-    // 리뷰 수정
     @PutMapping("/{reviewId}/updateReview")
+    @Operation(summary = "게시글 리뷰 수정")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> updateReview(@PathVariable("reviewId") Long reviewId, @RequestBody ReviewUpdateRequestDto requestDto) {
         return postService.updateReview(requestDto, reviewId);
     }
