8d5998092226570519e44cdf92fdff9b87bdd44a
GiYeons <65033360+GiYeons@users.noreply.github.com>
2023-07-15 19:41:31 +0900

    feat: 검색쿼리 구현(위치로직 제외)

--- src/main/java/kr/co/suitcarrier/web/dto/PostResponseDto.java
+++ src/main/java/kr/co/suitcarrier/web/dto/PostResponseDto.java
@@ -5,21 +5,26 @@ import kr.co.suitcarrier.web.entity.post.Post;
 import kr.co.suitcarrier.web.entity.post.PostState;
 import kr.co.suitcarrier.web.entity.post.Product;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Data
 @Setter(AccessLevel.NONE)
-@Schema(description = "게시글 조회(카드형)")
-public class PostCardResponseDto {
+@Schema(description = "게시글 상세조회")
+public class PostResponseDto {
     String title;
     String description;
     int price;
     int additionalPrice;
 
-    @Builder
-    public PostCardResponseDto(Post post) {
+    String state;
+
+    String color;
+    String brand;
+    String size;
+
+    public PostResponseDto(Post post) {
         PostState postState = post.getPostState();
         Product product = post.getProduct();
 
@@ -27,5 +32,9 @@ public class PostCardResponseDto {
         this.description = post.getDescription();
         this.price = post.getPrice();
         this.additionalPrice = post.getAdditionalPrice();
+        this.state = String.valueOf(postState.getState());
+        this.color = product.getColor();
+        this.brand = product.getBrand();
+        this.size = product.getSize();
     }
 }
