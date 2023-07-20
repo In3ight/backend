3b99ed9868e95a493226c14386672a6fe69beebf
songjaeheon <spes2you@gmail.com>
2023-07-20 14:09:14 +0900

    feat: auth controller api 선언 및 관련 코드, csrf 코드 삭제

--- src/main/java/kr/co/suitcarrier/web/repository/UserRepository.java
+++ src/main/java/kr/co/suitcarrier/web/repository/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import kr.co.suitcarrier.web.entity.User;
@@ -18,4 +19,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
     int countByEmail(String email);
     // 전화번호로 검색(회원가입 시 중복 가입 번호 목적으로 사용)
     int countByContact(String contact);
+
+    // uer 테이블에서 enabled 컬럼을 0으로 업데이트
+    @Query(value = "UPDATE user SET enabled = 0 WHERE id = :id", nativeQuery = true)
+    void updateEnabledZeroById(Long id);
+    // uer 테이블에서 enabled 컬럼을 1로 업데이트
+    @Query(value = "UPDATE user SET enabled = 1 WHERE id = :id", nativeQuery = true)
+    void updateEnabledOneById(Long id);
+    
 }
